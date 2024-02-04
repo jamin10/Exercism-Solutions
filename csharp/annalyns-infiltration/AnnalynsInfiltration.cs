ae951ccf109e259b0e8e85addd4301ec8cc935bf
Ben Whitfield <bpwhitfield@outlook.com>
2024-02-04 12:39:16 +0000

    Annalyns Infiltration solution

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,42 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+         if (knightIsAwake == true) {
+             return false;
+         } else {
+             return true;
+         }
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (archerIsAwake == false && prisonerIsAwake == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent == true && archerIsAwake == false) {
+            return true; 
+        } else if (petDogIsPresent == false && prisonerIsAwake == true && knightIsAwake == false && archerIsAwake == false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

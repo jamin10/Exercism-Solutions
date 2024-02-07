93beb7d627fe44e7f476e533c81a51d025e494df
Ben Whitfield <bpwhitfield@outlook.com>
2024-02-07 18:22:00 +0000

    Squeaky clean solution

---
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        string control ="";
+        for (int i = 0; i < identifier.Length; i++)
+        {
+            if (identifier[i] == ' ') {
+                control += '_';
+            } else if (char.IsControl(identifier[i])) {
+                control += "CTRL";
+            } else if (identifier[i] == '-') {
+                i ++;
+                control += char.ToUpper(identifier[i]);
+            } else if (!char.IsLetter(identifier[i]))
+            {
+            } else if (identifier[i] >= 'α' && identifier[i] <= 'ω')
+            {
+            } else
+            {
+                control += identifier[i];
+            }
+        }
+        return control;
+    }
+}

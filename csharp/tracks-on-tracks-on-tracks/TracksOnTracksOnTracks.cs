914bcdf114101e37f9d65d626b20d5a717c07e38
Ben Whitfield <bpwhitfield@outlook.com>
2024-02-25 18:34:58 +0000

    Tracks on tracks

---
+++ csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+public static class Languages
+{
+    public static List<string> NewList()
+    {
+        return new List<string>();
+    }
+
+    public static List<string> GetExistingLanguages()
+    {
+        return ["C#", "Clojure", "Elm"];
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        languages.Add(language);
+        return languages;
+    }
+
+    public static int CountLanguages(List<string> languages)
+    {
+        throw new NotImplementedException("Please implement the static Languages.CountLanguages() method");
+    }
+
+    public static bool HasLanguage(List<string> languages, string language)
+    {
+        throw new NotImplementedException("Please implement the static Languages.HasLanguage() method");
+    }
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        throw new NotImplementedException("Please implement the static Languages.ReverseList() method");
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        throw new NotImplementedException("Please implement the static Languages.IsExciting() method");
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        throw new NotImplementedException("Please implement the static Languages.RemoveLanguage() method");
+    }
+
+    public static bool IsUnique(List<string> languages)
+    {
+        throw new NotImplementedException("Please implement the static Languages.IsUnique() method");
+    }
+}

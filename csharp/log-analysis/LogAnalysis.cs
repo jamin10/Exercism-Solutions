7d906fdddc211deec911f0b108e898e7369cd807
Ben Whitfield <bpwhitfield@outlook.com>
2024-03-11 18:15:14 +0000

    Log Analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -3,6 +3,10 @@ using System;
 public static class LogAnalysis 
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string log, string delim)
+    {
+        return log.Split(delim)[1];
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     

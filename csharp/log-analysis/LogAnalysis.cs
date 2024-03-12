73c4b1ec3538c585cf0c59d889fd514cbbf855f8
Ben Whitfield <bpwhitfield@outlook.com>
2024-03-12 20:58:33 +0000

    Log analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -9,8 +9,20 @@ public static class LogAnalysis
     }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string log, string delim, string delim2)
+    {
+        return log.Split(delim)[1].Split(delim2)[0];
+    }
+
     // TODO: define the 'Message()' extension method on the `string` type
+    public static string Message(this string log)
+    {
+        return log.SubstringAfter(": ");
+    }
 
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string log)
+    {
+        return log.SubstringBetween("[", "]");
+    }
 }

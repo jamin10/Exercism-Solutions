32b74a311c3b110b2f78519bbc57bb327cd34ca1
Ben Whitfield <bpwhitfield@outlook.com>
2024-03-29 19:31:26 +0000

    Binary search

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Linq;
 
 public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double length = (double)input.Length;
+        double preIndex = (length - 1) / 2;
+        int index = (int)Math.Round(preIndex, 0, MidpointRounding.AwayFromZero);
+        if (input[index] == value)
+        {
+            return index;
+        } else if (input[index] < value)
+        {
+            int[] newInput = input.Skip(index).ToArray();
+            return Find(newInput, value);
+        } else if (input[index] > value)
+        {
+            int[] newInput = input.Take(index + 1).ToArray();
+            return Find(newInput, value);
+        } else
+        {
+            return value;
+        }
     }
 }
